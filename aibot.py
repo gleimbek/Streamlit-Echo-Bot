4bc7ebbbe132b83523342db75d38b122558c4d53
Gustavo Leimbek <143670858+gleimbek@users.noreply.github.com>
2026-03-05 17:40:54 -0800

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -5,7 +5,7 @@ import time
 import requests
 import json
 
-def ai_ask(prompt, data=None, temperature=0.5, max_tokens=250, model="mistral-small-latest", api_key=R1NoJ9fphmYOJd2YSZuhbc3AyyhKKC6j, api_url="https://api.mistral.ai/v1/chat/completions"):
+def ai_ask(prompt, data=None, temperature=0.5, max_tokens=250, model="mistral-small-latest", api_key=None, api_url="https://api.mistral.ai/v1/chat/completions"):
     if api_key is None or api_url is None:
         if "idToken" in globals():
             api_key = globals()["idToken"]
